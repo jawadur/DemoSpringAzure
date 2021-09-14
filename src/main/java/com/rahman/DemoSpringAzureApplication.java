ff0bcce86bfad959e866a5b66461da326a901520
jawadur <33778381+jawadur@users.noreply.github.com>
2021-09-15 01:21:33 +0300

    Update DemoSpringAzureApplication.java

--- src/main/java/com/rahman/DemoSpringAzureApplication.java
+++ src/main/java/com/rahman/DemoSpringAzureApplication.java
@@ -3,6 +3,8 @@ package com.rahman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
 @RestController
